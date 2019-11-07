8a5617fbf65163aa064a5a347d78ec83534c010c
parveensindhwani <parveensindhwani89@gmail.com>
2019-11-07 18:06:00 +0530

    Create deployment.sh

---
+++ deployment.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+
+echo "Git Checkout"
+
+if [ ! -d "hello-world" ];then
+
+git clone https://github.com/parveensindhwani/hello-world.git
+
+echo "Directory not exist"
+
+cd hello-world
+
+else
+
+echo "Directory already there"
+
+cd hello-world
+git pull
+
+fi
+
+echo "Compilation"
+export JAVA_HOME=/usr/lib/jvm/java-1.8.0-openjdk-1.8.0.232.b09-0.el7_7.x86_64
+export MAVEN_HOME=/opt/apache-maven-3.6.2/bin
+export PATH=$PATH:$MAVEN_HOME:$JAVA_HOME
+mvn clean install package
+cd ..
+
+echo "Installing Apache Tomcat"
+
+if [ ! -d "apache-tomcat-8.5.47" ];then
+
+wget http://apachemirror.wuchna.com/tomcat/tomcat-8/v8.5.47/bin/apache-tomcat-8.5.47.tar.gz
+tar -xzvf apache-tomcat-8.5.47.tar.gz
+
+else
+
+echo "Tomact is already installed"
+
+fi
+
+
+echo "Deployment"
+
+ps aux |grep "apache-tomcat" |grep -v "grep"
+
+if [[ $? -ne 0 ]];then
+echo "Tomcat is not running"
+pwd
+cp hello-world/webapp/target/webapp.war apache-tomcat-8.5.47/webapps/
+apache-tomcat-8.5.47/bin/startup.sh
+
+else
+echo "Tomcat is running"
+apache-tomcat-8.5.47/bin/shutdown.sh
+cp hello-world/webapp/target/webapp.war apache-tomcat-8.5.47/webapps/
+apache-tomcat-8.5.47/bin/startup.sh
+
+fi
